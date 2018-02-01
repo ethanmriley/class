2c99d0d9fb5ef75e7b701c4d28d50497225a60d6
Ethan Riley <ethanmriley@gmail.com>
2018-02-01 11:40:07 -0500

    addded provided mm.c

---
+++ CompOrg/hw/hw1/mm.c
@@ -0,0 +1,91 @@
+/*****************************************************************************/
+/* Assignment 1: 2-D/ Matrix Multiplication **********************************/
+/* Name:     Ethan Riley     *************************************************/
+/*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+int **mm_alloc( int rows, int columns )
+{
+    /*
+     * hints: allocate an array of pointers to ints, then allocate
+     * an row/array of ints and assign each int pointer that row
+     * see slide 62
+     */
+    return NULL;
+}
+
+void mm_free( int **matrix, int rows, int columns )
+{
+}
+
+void mm_read( int **matrix, int rows, int columns )
+{
+}
+
+void mm_print( int **matrix, int rows, int columns )
+{
+}
+
+void mm_mult( int **m1, int m1_rows, int m1_cols,
+              int **m2, int m2_rows, int m2_cols,
+              int **results)
+{
+}
+
+int main()
+{
+    /*
+     * You must keep this function AS IS.
+     * You are not allowed to modify it!
+     */
+    int **matrix1 = NULL;
+    int **matrix2 = NULL;
+    int **results_matrix = NULL;
+
+    int m1_rows, m1_columns;
+    int m2_rows, m2_columns;
+
+    printf("How many rows are in the first matrix? ");
+    scanf("%d", &m1_rows);
+    printf("How many columns are in the first matrix? ");
+    scanf("%d", &m1_columns);
+
+    printf("How many rows are in the second matrix? ");
+    scanf("%d", &m2_rows);
+    printf("How many columns are in the second matrix? ");
+    scanf("%d", &m2_columns);
+
+    printf("\n");
+
+    if (m1_columns != m2_rows) {
+        printf("Invalid matrix multiplication!\n");
+        exit(-1);
+    }
+
+    matrix1 = mm_alloc(m1_rows, m1_columns);
+    matrix2 = mm_alloc(m2_rows, m2_columns);
+
+    results_matrix = mm_alloc(m1_rows, m2_columns);
+
+    mm_read( matrix1, m1_rows, m1_columns );
+    mm_read( matrix2, m2_rows, m2_columns );
+
+    printf("\n");
+
+    mm_mult(matrix1, m1_rows, m1_columns, matrix2, m2_rows, m2_columns, results_matrix);
+
+    mm_print( matrix1, m1_rows, m1_columns );
+    printf("\n");
+    printf("multiplied by\n\n");
+    mm_print( matrix2, m2_rows, m2_columns );
+    printf("\n");
+    printf("is: \n\n");
+    mm_print( results_matrix, m1_rows, m2_columns );
+    mm_free( matrix1, m1_rows, m1_columns );
+    mm_free( matrix2, m2_rows, m2_columns );
+    mm_free( results_matrix, m1_rows, m2_columns );
+
+    return 0;
+}
