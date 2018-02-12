3b6bb7a63259ea6be090e694e055cb7bb976e2d0
Ethan Riley <ethanmriley@gmail.com>
2018-02-12 13:10:06 -0500

    implemented a whole lot more stuff

--- NetProg/hw/hw1/tftp.c
+++ NetProg/hw/hw1/tftp.c
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define MAXPACKET 1000
 
@@ -42,6 +43,50 @@ struct server_socket {
 //functions
 //---------
 
+struct server_socket get_socket() 
+{
+    struct server_socket servsock;
+    unsigned int servaddr_len = sizeof(servsock.addr);
+
+    if ((servsock.sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) // ask for an ipv4 UDP socket 
+    {
+        perror("get_socket failed");                     // if we don't get it, print the error
+        exit(EXIT_FAILURE);                              // and exit          
+    }
+
+    bzero(&servsock.addr, servaddr_len);
+
+    // fill out sockaddr_in struct
+    servsock.addr.sin_family      = AF_INET;           // we're using ipv4
+    servsock.addr.sin_addr.s_addr = htonl(INADDR_ANY); // ip addr to listen on
+    servsock.addr.sin_port        = 0;                 // ask for a port from the kernel
+
+    if ( bind(servsock.sockfd, (struct sockaddr *) &servsock.addr, servaddr_len) == -1) 
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE); 
+    }
+
+    if(getsockname(servsock.sockfd, (struct sockaddr *) &servsock.addr, &servaddr_len) == -1)
+    {
+        perror("getsockname failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return servsock;
+}
+
+void send_err(int sockfd, struct sockaddr_in cliaddr, short err_code, char *err_msg)
+{
+    struct error ERR;
+
+    ERR.opcode =   htons(5);
+    ERR.err_code = htons(err_code);
+    strncpy(ERR.err_msg, err_msg, sizeof(ERR.err_msg));
+
+    sendto(sockfd, (void *) &ERR, sizeof(ERR), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
+}
+
 void send_ack(int sockfd, struct sockaddr_in cliaddr, short block_num)
 {
     struct acknowledgment ACK;
@@ -49,11 +94,97 @@ void send_ack(int sockfd, struct sockaddr_in cliaddr, short block_num)
     ACK.opcode = htons(4);
     ACK.block_num = htons(block_num);
 
-    sendto(sockfd, (void *) &ACK, 4, 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
+    sendto(sockfd, (void *) &ACK, sizeof(ACK), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
 }
 
-void handle_read(int sockfd, struct sockaddr_in cliaddr, char *packet)
+void handle_fopen_err(int sockfd, struct sockaddr_in cliaddr)
 {
+    short  err_code;
+    char   *err_msg;
+    
+    if(errno == 13)
+    {
+        err_code = 2;
+        err_msg  = "Access violation.";
+    }
+    else if(errno = 2)
+    {
+        err_code = 1;
+        err_msg  = "File not found.";
+    }
+    else
+    {
+        err_code = 0;
+        err_msg  = "fopen failed.";
+    }
+
+    send_err(sockfd, cliaddr, err_code, err_msg);
+    exit(0);
+}
+
+void send_data(int sockfd, struct sockaddr_in cliaddr, char *data, int bytes_read, short block_num)
+{
+    struct data DATA;
+
+    DATA.opcode = htons(3);
+    DATA.block_num = htons(block_num);
+    memcpy(DATA.data, data, bytes_read);
+
+    sendto(sockfd, (void *) &DATA, (4 + bytes_read), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
+}
+
+void handle_read(int sockfd, struct sockaddr_in cliaddr, int TID, char *filename)
+{
+    FILE *fp;
+    char data[512] = {0};
+    struct acknowledgment ACK;
+    unsigned int addrlen = sizeof(cliaddr);
+    int bytes_read;
+    int block_num = 0;
+
+    if((fp = fopen(filename, "r")) == NULL)
+        handle_fopen_err(sockfd, cliaddr);
+
+    while(feof(fp) == 0)
+    {
+        block_num++;
+        bytes_read = fread(data, 1, 512, fp);
+        send_data(sockfd, cliaddr, data, bytes_read, block_num);
+        
+        recvfrom(sockfd, (void *) &ACK, sizeof(ACK), 0, (struct sockaddr *) &cliaddr, &addrlen);
+
+        if(cliaddr.sin_port != TID)
+            send_err(sockfd, cliaddr, 5, "Unknown transfer ID.");
+    }
+}
+
+void init_read(int sockfd, struct sockaddr_in cliaddr, char *packet)
+{
+    int  TID = cliaddr.sin_port;
+    int  fname_len;
+    char filename[512] = {0};
+    char mode[20] = {0};
+
+    strncat(filename, (packet + 2), sizeof(filename));
+
+    fname_len = (int) strlen(filename);
+
+    //printf("%s\n", filename);
+
+    strncat(mode, (packet + 3 + fname_len), sizeof(mode));
+
+    //printf("%s\n", mode);
+
+    if(strcmp("octet", mode) != 0)
+    {
+        char err_msg[40];
+        printf("mode: %s not supported\n", mode);
+        snprintf(err_msg, sizeof(err_msg), "%s not supported", mode);
+        send_err(sockfd, cliaddr, 0, err_msg);
+        exit(0);
+    }
+    else
+        handle_read(sockfd, cliaddr, TID, filename);
 }
 
 void handle_write(int sockfd, struct sockaddr_in cliaddr, char *packet)
@@ -70,7 +201,6 @@ void handle_requests(int sockfd, struct sockaddr_in cliaddr, socklen_t cliaddr_l
 
     while(1)
     {
-        printf("waiting\n");
         mesg_len = recvfrom(sockfd, &packet, MAXPACKET, 0, (struct sockaddr *) &cliaddr, &cliaddr_len);
 
         if(mesg_len == -1)
@@ -84,60 +214,27 @@ void handle_requests(int sockfd, struct sockaddr_in cliaddr, socklen_t cliaddr_l
         if((opcode == 1 || opcode == 2) && fork() == 0) //fork on read and write requests
         {
             printf("forked\n");
+
+            struct server_socket newsock = get_socket();
             
             if(opcode == 1)
             {
-                handle_read(sockfd, cliaddr, packet);
+                init_read(newsock.sockfd, cliaddr, packet);
             }
-            else if(opcode == 2)
+            else
             {
-                handle_write(sockfd, cliaddr, packet);
+                handle_write(newsock.sockfd, cliaddr, packet);
             }
         }
     }
 }
 
-struct server_socket get_socket() 
-{
-    struct server_socket servsock;
-
-    if ((servsock.sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) // ask for an ipv4 UDP socket 
-    {
-        perror("get_socket failed");                     // if we don't get it, print the error
-        exit(EXIT_FAILURE);                              // and exit          
-    }
-
-    bzero(&servsock.addr, sizeof(servsock.addr));
-
-    // fill out sockaddr_in struct
-    servsock.addr.sin_family      = AF_INET;           // we're using ipv4
-    servsock.addr.sin_addr.s_addr = htonl(INADDR_ANY); // ip addr to listen on
-    servsock.addr.sin_port        = 0;                 // ask for a port from the kernel
-
-    if ( bind(servsock.sockfd, (struct sockaddr *) &servsock.addr, sizeof(servsock.addr)) == -1) 
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE); 
-    }
-
-    return servsock;
-}
-
 int main(int argc, char **argv)
 {
-    unsigned int servaddr_len;
     struct sockaddr_in   cliaddr;  // this will hold port, ip, etc info for our client
     struct server_socket servsock; // this will hold the server socket's file descriptor and sockaddr_in
 
     servsock = get_socket();
-
-    servaddr_len = sizeof(servsock.addr);
-
-    if(getsockname(servsock.sockfd, (struct sockaddr *) &servsock.addr, &servaddr_len) == -1)
-    {
-        perror("getsockname failed");
-        exit(EXIT_FAILURE);
-    }
 
     printf("port: %d\n", ntohs(servsock.addr.sin_port));
 
