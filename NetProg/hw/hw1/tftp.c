91b249b37de94ecb105704742b4d41587b15470e
Ethan Riley <ethanmriley@gmail.com>
2018-02-12 22:17:53 -0500

    idek anymore dude
    timeout is actually working now and i think im using it correctly
    
    still working perfectly with no delayed or dropped packets

--- NetProg/hw/hw1/tftp.c
+++ NetProg/hw/hw1/tftp.c
@@ -17,7 +17,7 @@
 //packet structs
 //--------------
 
-
+//data, plus an int to pass around the length of the data in data[]
 struct data {
     short opcode;
     short block_num;
@@ -36,6 +36,7 @@ struct error {
     char  err_msg[40];
 };
 
+//wrapper for sockaddr_in to make get_socket() easier to use
 struct server_socket {
     int sockfd;
     struct sockaddr_in addr;
@@ -45,11 +46,13 @@ struct server_socket {
 //functions
 //---------
 
+//SIGALRM handler for timeouts
 static void sig_alrm(int signo)
 {
     return;
 }
 
+//get a socket on a random available port and return the info in a server_socket struct
 struct server_socket get_socket() 
 {
     struct server_socket servsock;
@@ -83,6 +86,7 @@ struct server_socket get_socket()
     return servsock;
 }
 
+//easy wrapper for sending error messages
 void send_err(int sockfd, struct sockaddr_in cliaddr, short err_code, char *err_msg)
 {
     struct error ERR;
@@ -94,6 +98,7 @@ void send_err(int sockfd, struct sockaddr_in cliaddr, short err_code, char *err_
     sendto(sockfd, (void *) &ERR, sizeof(ERR), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
 }
 
+//wrapper for sending acknowledgments
 void send_ack(int sockfd, struct sockaddr_in cliaddr, short block_num)
 {
     struct acknowledgment ACK;
@@ -104,6 +109,7 @@ void send_ack(int sockfd, struct sockaddr_in cliaddr, short block_num)
     sendto(sockfd, (void *) &ACK, sizeof(ACK), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr));
 }
 
+//distinguish what failed in fopen and send the correct err_code
 void handle_fopen_err(int sockfd, struct sockaddr_in cliaddr)
 {
     short  err_code;
@@ -129,42 +135,44 @@ void handle_fopen_err(int sockfd, struct sockaddr_in cliaddr)
     exit(0);
 }
 
-void send_data(int sockfd, struct sockaddr_in cliaddr, char *data, int bytes_read, short block_num)
+void Sendto(int sockfd, void *packet, int len, int flags, struct sockaddr *cliaddr, unsigned int cliaddr_len)
 {
-    struct data DATA;
-
-    DATA.opcode = htons(3);
-    DATA.block_num = htons(block_num);
-    memcpy(DATA.data, data, bytes_read);
-
-    if(sendto(sockfd, (void *) &DATA, (4 + bytes_read), 0, (struct sockaddr *) &cliaddr, sizeof(cliaddr)) == -1)
+    if(sendto(sockfd, packet, len, flags, cliaddr, cliaddr_len) == -1)
     {
         perror("sendto failed");
         exit(EXIT_FAILURE);
     }
 }
 
-void wait_for_ack(int sockfd, struct sockaddr_in cliaddr, char *data, int bytes_read, short block_num)
+//wrapper to send data in a data packet
+void send_data(int sockfd, struct sockaddr_in cliaddr, char *data, int bytes_read, short block_num)
 {
+    struct data DATA;
     struct acknowledgment ACK;
-    unsigned int addrlen = sizeof(cliaddr);
-    short recv_block_num;
+    unsigned int cliaddr_len = sizeof(cliaddr);
     int timeout_count = 0;
 
-    while(1) //waiting for the /correct/ acknowledgment
+    DATA.opcode = htons(3);
+    DATA.block_num = htons(block_num);
+    memcpy(DATA.data, data, bytes_read);
+
+    Sendto(sockfd, (void *) &DATA, (4 + bytes_read), 0, (struct sockaddr *) &cliaddr, cliaddr_len);
+
+    while(1)
     {
         alarm(1);
 
-        if(recvfrom(sockfd, (void *) &ACK, sizeof(ACK), 0, (struct sockaddr *) &cliaddr, &addrlen) == -1)
+        //if /any/ packet arrives on our port, we continue past this
+        if(recvfrom(sockfd, (void *) &ACK, sizeof(ACK), 0, (struct sockaddr *) &cliaddr, &cliaddr_len) == -1)
         {
             if(errno == EINTR)
             {
                 timeout_count++;
-                if(timeout_count < 10)
+                if(timeout_count < 10) //while it's been less than 10 seconds, we retransmit the data packet
                 {
-                    send_data(sockfd, cliaddr, data, bytes_read, block_num);
+                    Sendto(sockfd, (void *) &DATA, (4 + bytes_read), 0, (struct sockaddr *) &cliaddr, cliaddr_len);
                 }
-                else if(timeout_count == 10)
+                else if(timeout_count == 10) //once it's been 10 seconds, we timeout and close the connection by killing the process
                 { 
                     printf("timed out waiting for acknowledgement.\n");
                     exit(0);
@@ -177,19 +185,20 @@ void wait_for_ack(int sockfd, struct sockaddr_in cliaddr, char *data, int bytes_
             }
         }
 
-        recv_block_num = ntohs(ACK.block_num);
+        alarm(0);
 
-        if(recv_block_num == block_num) //we've recieved the ack we were waiting for
-            return;
+        if(ACK.block_num == htons(block_num))
+            return; 
     }
 }
 
+//handles read operations by opening files, sending data, and receiving acknowledgments
 void handle_read(int sockfd, struct sockaddr_in cliaddr, char *filename)
 {
     FILE *fp;
     char data[512] = {0};
     int bytes_read;
-    short TID = cliaddr.sin_port;
+    int TID = cliaddr.sin_port;
     short block_num = 0;
 
     if((fp = fopen(filename, "r")) == NULL)
@@ -201,14 +210,13 @@ void handle_read(int sockfd, struct sockaddr_in cliaddr, char *filename)
         bytes_read = fread(data, 1, 512, fp);
 
         send_data(sockfd, cliaddr, data, bytes_read, block_num);
-        
-        wait_for_ack(sockfd, cliaddr, data, bytes_read, block_num);
 
         if(cliaddr.sin_port != TID)
             send_err(sockfd, cliaddr, 5, "Unknown transfer ID.");
     }
 }
 
+//essentially identical to wait_for_ack, but it calls send_ack instead of send_data
 struct data wait_for_data(int sockfd, struct sockaddr_in cliaddr, short block_num)
 {
     struct data DATA;
@@ -234,7 +242,6 @@ struct data wait_for_data(int sockfd, struct sockaddr_in cliaddr, short block_nu
                 {
                     printf("socket timed out while waiting for data\n");
                     exit(0);
-                
                 }
             }
             else
@@ -249,10 +256,11 @@ struct data wait_for_data(int sockfd, struct sockaddr_in cliaddr, short block_nu
         if(DATA.block_num == htons(block_num))
         {
             return DATA;
-        }
+        } 
     }
 }
 
+//function to handle write operations by opening files, receving data, and sending acknowledgments
 void handle_write(int sockfd, struct sockaddr_in cliaddr, char *filename)
 {
     FILE   *fp;
@@ -260,7 +268,7 @@ void handle_write(int sockfd, struct sockaddr_in cliaddr, char *filename)
     short  block_num = 0;
     struct data DATA;
 
-    if(access(filename, F_OK) == 0)
+    if(access(filename, F_OK) == 0) //check if the file we're trying to write already exists
     {
         send_err(sockfd, cliaddr, 6, "File already exists.");
         exit(0);
@@ -288,9 +296,9 @@ void handle_write(int sockfd, struct sockaddr_in cliaddr, char *filename)
     }
 }
 
+//handle both read and write requests. this is called on any incoming initial packet
 void handle_request(int sockfd, struct sockaddr_in cliaddr, socklen_t cliaddr_len)
 {
-
     int   mesg_len;
     char  packet[MAXPACKET];
     short opcode;
@@ -305,7 +313,7 @@ void handle_request(int sockfd, struct sockaddr_in cliaddr, socklen_t cliaddr_le
             exit(EXIT_FAILURE);
         }
 
-        opcode = ntohs(*((short *) packet));
+        opcode = ntohs(*((short *) packet)); //assuming well-formatted packets every time
 
         if((opcode == 1 || opcode == 2) && fork() == 0) //fork on read and write requests
         {
@@ -315,15 +323,12 @@ void handle_request(int sockfd, struct sockaddr_in cliaddr, socklen_t cliaddr_le
             char mode[20] = {0};
 
             strncat(filename, (packet + 2), sizeof(filename));
-
             fname_len = (int) strlen(filename);
-
             strncat(mode, (packet + 3 + fname_len), sizeof(mode));
 
             if(strcmp("octet", mode) != 0)
             {
                 char err_msg[40];
-                printf("%s not supported\n", mode);
                 snprintf(err_msg, sizeof(err_msg), "%s not supported", mode);
                 send_err(sockfd, cliaddr, 0, err_msg);
                 exit(0);
@@ -341,12 +346,16 @@ int main(int argc, char **argv)
 {
     struct sockaddr_in   cliaddr;  // this will hold port, ip, etc info for our client
     struct server_socket servsock; // this will hold the server socket's file descriptor and sockaddr_in
+    struct sigaction act;
+
+    act.sa_handler = sig_alrm;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGALRM, &act, NULL);
 
     servsock = get_socket();
 
-    printf("port: %d\n", ntohs(servsock.addr.sin_port));
-
-    signal(SIGALRM, sig_alrm);
+    printf("%d\n", ntohs(servsock.addr.sin_port));
 
     handle_request(servsock.sockfd, cliaddr, sizeof(cliaddr));
 }
