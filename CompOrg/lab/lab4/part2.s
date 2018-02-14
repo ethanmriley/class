0d7b3406e7c174704afb69294545a30d1848c88f
Ethan Riley <ethanmriley@gmail.com>
2018-02-14 14:37:58 -0500

    updated comporg lab 4 part 2 to be more sane

--- CompOrg/lab/lab4/part2.s
+++ CompOrg/lab/lab4/part2.s
@@ -1,11 +1,9 @@
-      .data
-offset: 
       .text
       .globl main
 main: li  $t1, 5
       li  $t0, 2
       slt $t2, $t0, $t1
-      bltz $t2, skip
+      beq $t2, $zero, skip
       add $t0, $t0, $t1
 skip: li  $v0, 1
       move $a0, $t0
