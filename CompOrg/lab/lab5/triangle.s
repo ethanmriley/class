d48aa475407d733358ee3cc92c5c61c1efab2013
Ethan Riley <ethanmriley@gmail.com>
2018-02-21 13:54:11 -0500

    finished lab 5

--- CompOrg/lab/lab5/triangle.s
+++ CompOrg/lab/lab5/triangle.s
@@ -1,6 +1,10 @@
     .data
 ask:
     .asciiz "What is n? "
+ast:
+    .asciiz "*"
+newline:
+    .asciiz "\n"
 
     .text
     .globl main
@@ -12,19 +16,36 @@ main:
     li $v0, 5 #read int to $v0
     syscall
 
-    move $t1, $v0 #store entered int in t1
-    li   $t0, 0   #use t0 as a counter, initialize to 0
+    move $t2, $v0 #store entered int in t1
+    li   $t1, 1   #use t1 as a counter, initialize to 1
     
-header:
-    beq $t0, $t1, done
-    j body
+header1:
+    bgt $t1, $t2, exit
+    li  $t0, 1
+    j header2
 
-body:
-   j latch 
+header2:
+    bgt $t0, $t1, latch1
+    j print
 
-latch:
-    addi $t0, $t0, 1 #increment our counter
-    j header
+print:
+    li $v0, 4
+    la $a0, ast
+    syscall
+    j latch2   
 
-done:
+latch2:
+    addi $t0, $t0, 1
+    j header2 
+
+latch1:
+    addi $t1, $t1, 1 #increment our counter
+
+    li $v0, 4
+    la $a0, newline    
+    syscall
+
+    j header1
+
+exit:
      jr $ra   
