aa823f92406ffefccecd436d62c233704755d374
Ethan Riley <ethanmriley@gmail.com>
2018-03-14 19:16:33 -0400

    functioning zeroconf echo server

--- NetProg/hw/hw2/zeroconf_echoserv.c
+++ NetProg/hw/hw2/zeroconf_echoserv.c
@@ -6,30 +6,35 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-struct sockaddr_in get_socket()
+struct server_socket 
     {
-    struct sockaddr_in rps_sock;
-    unsigned int rps_sock_len = sizeof(rps_sock);
-    int servfd;
-    if ((servfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) // ask for an ipv4 TCP socket 
+    int sockfd;
+    struct sockaddr_in sockaddr;
+    };
+
+struct server_socket get_socket()
+    {
+    struct server_socket rps_sock;
+    unsigned int sockaddr_len = sizeof(rps_sock.sockaddr);
+    if ((rps_sock.sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) // ask for an ipv4 TCP socket 
         {
         perror("get_socket failed");
         exit(EXIT_FAILURE);
         }
     
-    bzero(&rps_sock, rps_sock_len);
+    bzero(&rps_sock.sockaddr, sockaddr_len);
 
-    rps_sock.sin_family      = AF_INET;
-    rps_sock.sin_addr.s_addr = htonl(INADDR_ANY);
-    rps_sock.sin_port        = 0;
+    rps_sock.sockaddr.sin_family      = AF_INET;
+    rps_sock.sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    rps_sock.sockaddr.sin_port        = 0;
     
-    if (bind(servfd, (struct sockaddr*)&rps_sock, rps_sock_len) == -1)
+    if (bind(rps_sock.sockfd, (struct sockaddr*)&rps_sock.sockaddr, sockaddr_len) == -1)
         {
         perror("bind failed");
         exit(EXIT_FAILURE);
         }
 
-    if (getsockname(servfd, (struct sockaddr*)&rps_sock, &rps_sock_len) == -1)
+    if (getsockname(rps_sock.sockfd, (struct sockaddr*)&rps_sock.sockaddr, &sockaddr_len) == -1)
         {
         perror("getsockname failed");
         exit(EXIT_FAILURE);
@@ -59,7 +64,7 @@ MyRegisterCallBack(DNSServiceRef service,
 
 int main (int argc, const char * argv[])
 	{
-    struct sockaddr_in rps_sock = get_socket();
+    struct server_socket rps_sock = get_socket();
 
     DNSServiceErrorType error;
     DNSServiceRef serviceRef;
@@ -70,15 +75,15 @@ int main (int argc, const char * argv[])
 								"_rps._tcp",       // service type
 								"",                 // register in default domain(s)
 								NULL,               // use default host name
-								rps_sock.sin_port,        // port number
+								rps_sock.sockaddr.sin_port,        // port number
 								0,                  // length of TXT record
 								NULL,               // no TXT record
 								MyRegisterCallBack, // call back function
 								NULL);              // no context
     printf("DNSServiceRegister returned %d\n", error);
 
-    int rpsfd = DNSServiceRefSockFD(serviceRef);
-    int nfds = rpsfd + 1;
+    int daemonfd = DNSServiceRefSockFD(serviceRef);
+    int nfds = daemonfd + 1;
     fd_set readfds;
     struct timeval tv;
     int result;
@@ -86,19 +91,39 @@ int main (int argc, const char * argv[])
     while(1) 
         {
         FD_ZERO(&readfds);
-        FD_SET(rpsfd, &readfds);
+        FD_SET(daemonfd, &readfds);
+        FD_SET(rps_sock.sockfd, &readfds);
         tv.tv_sec = 1000000;
         tv.tv_usec = 0;
+
+        listen(rps_sock.sockfd, 20);
 
         result = select(nfds, &readfds, (fd_set*)NULL, (fd_set*)NULL, &tv);
         if (result > 0) 
             {
             DNSServiceErrorType err = kDNSServiceErr_NoError;
-            if (FD_ISSET(rpsfd, &readfds))
+            if (FD_ISSET(daemonfd, &readfds))
                 {
                 err = DNSServiceProcessResult(serviceRef);
                 }
+            
+            if (FD_ISSET(rps_sock.sockfd, &readfds))
+                {
+                char buf[] = "Hello from the server!!!\n";
+                struct sockaddr_in client;
+                int cliaddr_len = sizeof(client);
+                int new_sock = accept(rps_sock.sockfd, (struct sockaddr*)&client, &cliaddr_len);
+                send(new_sock, &buf, sizeof(buf), 0); //say hello
+                while(1)//echo infinitely
+                    {
+                    char msg[40];
+                    bzero(&msg, sizeof(msg));
+                    recv(new_sock, msg, sizeof(msg), 0);
+                    send(new_sock, &msg, sizeof(msg), 0);
+                    }    
+                }
             }
+
         else 
             {
             printf("select() returned %d\n", result);
