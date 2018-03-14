36a84a9022fa5dc048227a7e2d2bf66cf4084884
Ethan Riley <ethanmriley@gmail.com>
2018-03-14 16:34:18 -0400

    added hw2 files

---
+++ NetProg/hw/hw2/reg_service.c
@@ -0,0 +1,42 @@
+#include <dns_sd.h>
+#include <stdio.h>			// For stdout, stderr
+#include <string.h>			// For strlen(), strcpy(), bzero()
+#include <arpa/inet.h>
+
+static void
+MyRegisterCallBack(DNSServiceRef service,
+				   DNSServiceFlags flags,
+				   DNSServiceErrorType errorCode,
+				   const char * name,
+				   const char * type,
+				   const char * domain,
+				   void * context)
+	{
+	#pragma unused(flags)
+	#pragma unused(context)
+
+	if (errorCode != kDNSServiceErr_NoError)
+		fprintf(stderr, "MyRegisterCallBack returned %d\n", errorCode);
+	else
+		printf("%-15s %s.%s%s\n","REGISTER", name, type, domain);
+	}
+
+int main (int argc, const char * argv[])
+	{
+    DNSServiceErrorType error;
+    DNSServiceRef serviceRef;
+	error = DNSServiceRegister(&serviceRef,
+								0,                  // no flags
+								0,                  // all network interfaces
+								"riley4",  // name
+								"_rps._tcp",       // service type
+								"",                 // register in default domain(s)
+								NULL,               // use default host name
+								htons(9092),        // port number
+								0,                  // length of TXT record
+								NULL,               // no TXT record
+								MyRegisterCallBack, // call back function
+								NULL);              // no context
+	fprintf(stderr, "DNSServiceDiscovery returned %d\n", error);
+	return 0;
+	}
