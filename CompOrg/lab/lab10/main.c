b14e2c1fc76271e7a74cb5699a31ca272a91b4ac
Ethan Riley <ethanmriley@gmail.com>
2018-04-11 14:31:24 -0400

    finished lab 10

--- CompOrg/lab/lab10/main.c
+++ CompOrg/lab/lab10/main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <assert.h>
 #include <math.h>
+#include <stdlib.h>
 
 /*
  Given the index values, determine the number of slots in your cache.
@@ -27,12 +28,32 @@ uint32_t slot_count(uint32_t idx_start, uint32_t idx_end)
  
  this function should return 0110 1001 or 0x69 or 105 in decimal.
  */
+
+int get_bit(uint32_t num, int index) {
+    return (num & (1 << index)) >> index;
+}
+
 uint32_t get_index(uint32_t address, uint32_t idx_start, uint32_t idx_end)
 {
     // assert() calls are great to ensure your logic is valid!
     assert(idx_start < idx_end);
 
-    return 0;
+    int bit;
+    char index[20] = {'\30'};
+    int i = idx_end;
+    int j = 0;
+
+    for(; i >= idx_start; i--) {
+        bit = get_bit(address, i);
+        if(bit == 1) {
+            index[j] = '1';
+        } else {
+            index[j] = '0';
+        }
+        j++;
+    }
+
+    return strtoul(index, NULL, 2);
 }
 
 /*
@@ -44,10 +65,13 @@ uint32_t get_index(uint32_t address, uint32_t idx_start, uint32_t idx_end)
 uint32_t get_cache_line_size(uint32_t idx_start, uint32_t idx_end, uint32_t bob)
 {
     // assert() calls are great to ensure your logic is valid!
+    int data;
+    data = pow(2, bob+1);
+    data *= 8;
     assert(bob + 1 == idx_start);
     assert(idx_start < idx_end);
     
-    return 0;
+    return (32 - idx_end + data + 1);
 }
 
 int main()
@@ -67,8 +91,8 @@ int main()
            get_index(0x2135C690, 5, 17));
     
     // This should return 49
-    printf("get_cache_line_size(3, 16, 2) returned %d\n\n",
-           get_cache_line_size(3, 16, 2));
+    printf("get_cache_line_size(2, 16, 1) returned %d\n\n",
+           get_cache_line_size(2, 16, 1));
     
     printf("get_cache_line_size(4, 15, 3) returned %d\n\n",
            get_cache_line_size(4, 15, 3));
