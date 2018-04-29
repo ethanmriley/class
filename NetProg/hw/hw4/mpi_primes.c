113469a0a73a91d22e8730e950c5311c94033578
Ethan Riley <ethanmriley@gmail.com>
2018-04-29 15:32:20 -0400

    added find_primes()

--- NetProg/hw/hw4/mpi_primes.c
+++ NetProg/hw/hw4/mpi_primes.c
@@ -13,6 +13,24 @@ void sig_handler(int signo)
     }
 }
 
+void find_primes() {
+    int prime_count = 1; //accounting for the fact that 2 is prime
+    for(unsigned int i = 3; i <= 1000; i++) {
+        int prime = 1;
+
+        for(unsigned int j = 2; j <= (int) ceil(sqrt( (double) i)); j++) {
+            if(i % j == 0) {
+                prime = 0;
+                break;
+            }
+        }
+
+        if(prime) prime_count++;
+
+        if(i == 10 || i == 100 || i == 1000) printf("%d %d\n", i, prime_count);
+    }
+}
+
 int main(int argc, char **argv)
 {
     int count, id;
@@ -20,6 +38,8 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &count);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    printf("rank: %d\n", id);
     
     signal(SIGUSR1, sig_handler);
     
