d5fc92ce4e90db39cebbcfda076c154d95ae453a
Ethan Riley <ethanmriley@gmail.com>
2018-04-29 17:03:48 -0400

    added algorithm to chunk up the range

--- NetProg/hw/hw4/mpi_primes.c
+++ NetProg/hw/hw4/mpi_primes.c
@@ -18,7 +18,7 @@ void find_primes() {
     for(unsigned int i = 3; i <= 1000; i++) {
         int prime = 1;
 
-        for(unsigned int j = 2; j <= (int) ceil(sqrt( (double) i)); j++) {
+        for(unsigned int j = 2; j <= (int) ceil(sqrt(i)); j++) {
             if(i % j == 0) {
                 prime = 0;
                 break;
@@ -34,20 +34,43 @@ void find_primes() {
 int main(int argc, char **argv)
 {
     int count, id;
+    unsigned int i = 0;
     
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &count);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-    printf("rank: %d\n", id);
-    
-    signal(SIGUSR1, sig_handler);
-    
-    while (1) {
-        if (end_now == 1) {
-            break;
+    for(i = 3; i < 100; i++) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        printf("\n");
+
+        int sqrt_i = floor(sqrt(i));
+        int range = (sqrt_i - 1);
+        int chunk_size = ceil(range/ (float)count);
+        int start = 2 + (chunk_size * (id));
+        int end;
+
+        if(id == 3) {
+            end = sqrt_i + 1;
+        } else {
+            end = start + chunk_size;
         }
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        printf("rank: %d i: %d start: %d end: %d sqrt_i: %d chunk_size: %d\n", id, i, start, end, sqrt_i, chunk_size);
+        
     }
+
+    signal(SIGUSR1, sig_handler);
+
+    //i need to wait until all processes are done until i move to the next candidate
+    //each process should check the numbers between (int) ceil(i/count) * id and (int) ceil(i/couint) * (id + 1)
+    
+    //while (1) {
+
+
+    //    if (end_now == 1) break;
+    //}
     
     MPI_Finalize();
     
